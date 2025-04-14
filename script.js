1b1ba55f820ee1a5f6f01a2b9e7a3b5cd6ac3fbc
Skander Hassan <skander.hs14@gmail.com>
2025-04-14 16:22:01 +0100

    Ajout fonction addTask pour ajouter une tâche

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+console.log("Todo app en cours de développement...");
+const tasks = [];
 
+function addTask(task) {
+  tasks.push(task);
+  console.log("Tâche ajoutée :", task);
+}
+
+addTask("Faire les courses");
